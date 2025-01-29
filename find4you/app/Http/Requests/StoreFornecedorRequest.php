6ab2e9fdf4281aec04083d01207bcb0c6a06a3f8
wfreitass <wiltterfreitasw@gmal.com>
2025-01-29 15:44:39 -0300

    validation cpf cnpj

--- find4you/app/Http/Requests/StoreFornecedorRequest.php
+++ find4you/app/Http/Requests/StoreFornecedorRequest.php
@@ -28,7 +28,11 @@ class StoreFornecedorRequest extends FormRequest
         return [
             'nome' => 'required|max:255',
             'email' => 'required|unique:fornecedores|max:255',
-            'cpf_cnpj' => 'required|unique:fornecedores|max:255',
+            'cpf_cnpj' => ['required', 'string', 'max:18', function ($attribute, $value, $fail) {
+                if (!$this->validaCpfCnpj($value)) {
+                    $fail('O campo CPF/CNPJ não é válido.');
+                }
+            }],
         ];
     }
 
@@ -61,5 +65,75 @@ class StoreFornecedorRequest extends FormRequest
             'cpf_cnpj.unique' => 'cpf_cnpj já cadastrado',
 
         ];
+    }
+
+    private function validaCpfCnpj($value): bool
+    {
+        $cpfCnpj = preg_replace('/\D/', '', $value);
+
+        if (strlen($cpfCnpj) === 11) {
+            return $this->validaCpf($cpfCnpj);
+        } elseif (strlen($cpfCnpj) === 14) {
+            return $this->validaCnpj($cpfCnpj);
+        }
+
+        return false;
+    }
+
+    private function validaCpf($cpf): bool
+    {
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+
+        for ($t = 9; $t < 11; $t++) {
+            $d = 0;
+            for ($c = 0; $c < $t; $c++) {
+                $d += $cpf[$c] * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private function validaCnpj($cnpj): bool
+    {
+        if (preg_match('/(\d)\1{13}/', $cnpj)) {
+            return false;
+        }
+
+        $tamanho = strlen($cnpj) - 2;
+        $numeros = substr($cnpj, 0, $tamanho);
+        $digitos = substr($cnpj, $tamanho);
+        $soma = 0;
+        $pos = $tamanho - 7;
+        for ($i = $tamanho; $i >= 1; $i--) {
+            $soma += $numeros[$tamanho - $i] * $pos--;
+            if ($pos < 2) {
+                $pos = 9;
+            }
+        }
+
+        $resultado = $soma % 11 < 2 ? 0 : 11 - $soma % 11;
+        if ($resultado != $digitos[0]) {
+            return false;
+        }
+
+        $tamanho += 1;
+        $numeros = substr($cnpj, 0, $tamanho);
+        $soma = 0;
+        $pos = $tamanho - 7;
+        for ($i = $tamanho; $i >= 1; $i--) {
+            $soma += $numeros[$tamanho - $i] * $pos--;
+            if ($pos < 2) {
+                $pos = 9;
+            }
+        }
+
+        $resultado = $soma % 11 < 2 ? 0 : 11 - $soma % 11;
+        return $resultado == $digitos[1];
     }
 }
